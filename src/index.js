4cca0d3730d6a39dac6e9856ae6b055342fb2f11
Victor Vuelma <victorvuelma@outlook.com>
2019-12-14 18:04:35 -0300

    setup babel-root-import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { SafeAreaView, Text, StatusBar } from 'react-native';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>Hey!</Text>
       </SafeAreaView>
