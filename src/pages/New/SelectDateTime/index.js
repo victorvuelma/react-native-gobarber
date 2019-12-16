55f6aca6e2f19aa9e71236bb1e0b3ad45dfe48b7
Victor Vuelma <victorvuelma@outlook.com>
2019-12-15 23:49:50 -0300

    change back icon on SelectDateTime page

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
@@ -9,4 +13,13 @@ export default function SelectDateTime() {
 
 SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
 });
