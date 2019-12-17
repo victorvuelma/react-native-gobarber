1f26dfc791725edc67f5bb943bb6900be313dc25
Victor Vuelma <victorvuelma@outlook.com>
2019-12-16 23:42:24 -0300

    get provider availability from api

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 
+import { useNavigation } from 'react-navigation-hooks';
+
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
@@ -9,7 +13,26 @@ import DateInput from '~/components/DateInput';
 import { Container } from './styles';
 
 export default function SelectDateTime() {
+  const navigation = useNavigation();
+
+  const provider = navigation.getParam('provider');
+
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
 
   return (
     <Background>
